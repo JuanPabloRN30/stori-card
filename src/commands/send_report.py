5c18c4d1e25b57b1d7ad6122d57f0f6649d73569
Juan Pablo Rodríguez Navarro <juanpablorn30@gmail.com>
2023-05-26 21:26:06 -0500

    feature: Update send_report command to include SQLReportHandler (#11)

--- src/commands/send_report.py
+++ src/commands/send_report.py
@@ -1,7 +1,9 @@
 import click
 
+from boostrap import get_session
+from constants import DATABASE_URL
 from emails import Email
-from handlers import InMemoryReportHandler
+from handlers import InMemoryReportHandler, SQLReportHandler
 from models import GenerateReportCommand
 from notifications import EmailReportNotification
 from process import process_transaction_file
@@ -12,9 +14,20 @@ from process import process_transaction_file
 @click.argument("email", type=click.STRING)
 def send_report(filepath: str, email: str) -> None:
     command = GenerateReportCommand(filepath=filepath, receiver_email=email)
-    process_transaction_file(
-        InMemoryReportHandler(), EmailReportNotification(Email), command
-    )
+
+    if DATABASE_URL:
+        session = get_session()
+        process_transaction_file(
+            SQLReportHandler(session), EmailReportNotification(Email), command
+        )
+        session.commit()
+        session.close()
+    else:
+        process_transaction_file(
+            InMemoryReportHandler(), EmailReportNotification(Email), command
+        )
+
+    click.echo("Report sent!")
 
 
 if __name__ == "__main__":
